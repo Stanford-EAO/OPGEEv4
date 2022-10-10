9b7f29a5c5d551c451fcfda4ca88e9189bab30f8
spencer.zhang <spencerzhang99@gmail.com>
2022-10-09 22:42:38 -0700

    update combine_streams

--- tests/test_combine_streams.py
+++ tests/test_combine_streams.py
@@ -3,7 +3,7 @@ from opgee.core import TemperaturePressure
 from opgee.stream import Stream
 from opgee import ureg
 from opgee.combine_streams import combine_streams, combine_thm_streams
-from thermosteam import Chemical, Chemicals, Stream as str
+from thermosteam import Chemical, Chemicals, MultiStream
 import thermosteam as tmo
 from opgee.table_manager import TableManager
 
@@ -15,7 +15,7 @@ def test_combine_streams():
     s = Stream("test_stream2", test_tp2)
     s.set_flow_rate("N2", "gas", 525.318807)
     s.set_flow_rate("CO2", "gas", 5594.456607)
-    s.set_flow_rate("CH4", "gas", 354.916584)
+    s.set_flow_rate("C1", "gas", 354.916584)
     s.set_flow_rate("C2", "gas", 74.644754)
     s.set_flow_rate("C3", "gas", 49.183948)
     s.set_flow_rate("C4", "gas", 38.116475)
@@ -31,25 +31,23 @@ df = TableManager().get_table("composite-oil")
 chemicals = Chemicals({name: Chemical(name) for name in df.index}, cache=True)
 tmo.settings.set_thermo(chemicals, cache=True)
 
-s1 = str(ID="s1",
-         phase='g',
-         CO2=2770.14636,
+s1 = MultiStream(ID="s1",
+         g=[("CO2",2770.14636)],
          T=90,
          P=500)
 
-# s2 = str(ID="s2",
-#          phase='g',
-#          N2=525.318807,
-#          CO2=5594.456607,
-#          CH4=354.916584,
-#          C2=74.644754,
-#          C3=49.183948,
-#          i-C4=38.116475,
-#          T=60,
-#          P=165)
-#
-#
-# def test_combine_thm_streams():
-#     s_sum = combine_thm_streams([s1,s2])
-#     total_flow = ureg.Quantity(s_sum.get_flow('t/d').sum(), 't/d')
-#     assert total_flow == ureg.Quantity(pytest.approx(9483.849, rel=1e-2), "t/d")
+s2 = MultiStream(ID="s2",
+        g=[("N2",525.318807),
+         ("CO2",5594.456607),
+         ("CH4",354.916584),
+         ("C2",74.644754),
+         ("C3",49.183948),
+         ("i-C4",38.116475)],
+         T=60,
+         P=165)
+
+
+def test_combine_thm_streams():
+    s_sum = combine_thm_streams([s1,s2])
+    total_flow = ureg.Quantity(s_sum.get_flow('t/d').sum(), 't/d')
+    assert total_flow == ureg.Quantity(pytest.approx(9483.849, rel=1e-2), "t/d")
